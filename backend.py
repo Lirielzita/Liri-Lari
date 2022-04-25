ef707bc1faf3ddb39160e919ca6c33ada715eeeb
Lirielzita <101529138+Lirielzita@users.noreply.github.com>
2022-04-25 10:35:49 -0300

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,24 @@
+from config import *
+from modelo import Pessoa
+
+@app.route("/")
+def inicio():
+    return 'Sistema de cadastro de pessoas. '+\
+        '<a href="/listar_pessoas">Operação listar</a>'
+
+@app.route("/retornar_pessoas")
+def retornar_pessoas():
+    # obter as pessoas do cadastro
+    pessoas = db.session.query(Pessoa).all()
+    # aplicar o método json que a classe Pessoa possui a cada elemento da lista
+    pessoas_em_json = [ x.json() for x in pessoas ]
+    # converter a lista do python para json
+    resposta = jsonify(pessoas_em_json)
+    # PERMITIR resposta para outras pedidos oriundos de outras tecnologias
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta # retornar...
+
+app.run(debug=True)
+
+# teste da API:
+# curl localhost:5000/retornar_pessoas
--- config.py
+++ config.py
@@ -1,13 +1,15 @@
-from flask import Flask
+
+# importações
+from flask import Flask, jsonify # preparar resposta HTTP no formato json
 from flask_sqlalchemy import SQLAlchemy
 import os
 
 # configurações
-app = Flask(__name__) # vínculo com o Flask
+app = Flask(__name__)
 # caminho do arquivo de banco de dados
-path = os.path.dirname(os.path.abspath(__file__))
-arquivobd = os.path.join(path, 'Larissa-Liriel.db')
+path = os.path.dirname(os.path.abspath(__file__)) 
+arquivobd = os.path.join(path, 'Lari-Liri.db')
 # sqlalchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///"+arquivobd
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # remover warnings
-db = SQLAlchemy(app) # vínculo com o SQLAlchemy
+db = SQLAlchemy(app)
